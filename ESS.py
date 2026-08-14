4a9a0b5f122c3edad889fb0c3579fbe11ec8224d
aouanoukgaya2006@outlook.com <tonemail@exemple.com>
2026-08-14 12:18:57 +0200

    Version finale du projet

--- ESS.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd  
-
-data = pd.read_csv("train.csv")
-
-data["Age"] = data["Age"].fillna(data["Age"].median())
-data["Embarked"] = data["Embarked"].fillna(data["Embarked"].mode()[0])
